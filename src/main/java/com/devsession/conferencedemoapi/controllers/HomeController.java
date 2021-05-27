21e21b7a067a24624649548c48b22d356ae71ac7
jmayorga94 <javi.mayorga1994@gmail.com>
2021-05-26 23:38:22 -0600

    Fix homeController getVersion method

--- src/main/java/com/devsession/conferencedemoapi/controllers/HomeController.java
+++ src/main/java/com/devsession/conferencedemoapi/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.devsession.conferencedemoapi.controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,7 +16,7 @@ public class HomeController {
     private String appVersion;
 
     @GetMapping
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET, path = "/", produces = "application/json")
     public Map getVersion(){
         Map map = new HashMap<String,String>();
         map.put("app-version",appVersion);
