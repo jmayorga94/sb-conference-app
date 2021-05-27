fd7ba1e82b434c70da93532f727a3cd66afc561e
jmayorga94 <javi.mayorga1994@gmail.com>
2021-05-26 23:03:26 -0600

    Changed API name

--- src/main/java/com/devsession/conferencedemoapi/config/SpringFoxConfig.java
+++ src/main/java/com/devsession/conferencedemoapi/config/SpringFoxConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SpringFoxConfig {
@@ -13,8 +16,13 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .paths(PathSelectors.ant("/api/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.devsessions"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Conference API", "Demo conference app", "1.0","public api",new springfox.documentation.service.Contact("Javier Mayorga","http://devsessions.io","javi.mayorga1994@outlook.com"),"API License","http://devsessions.io", Collections.emptyList());
     }
 }
